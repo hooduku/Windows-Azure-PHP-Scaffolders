aa37258f166a1fa7bc784c5b71f8afc2f3b3687a
sseshachala <sid4it@gmail.com>
2012-01-05 16:24:39 -0600

    Added additional parameters.

--- Joomla/Joomla/Config.class.php
+++ Joomla/Joomla/Config.class.php
@@ -2,8 +2,13 @@
 
 class Config {
     
-	public $joomla_link = "http://joomlacode.org/gf/download/frsrelease/16287/70842/Joomla_2.5.0_Beta1-Full_Package.zip";
-	public $cdn_link    = "http://hwazstor1.blob.core.windows.net/cdn/CDN.zip";
-
+	public $joomla_link 	= "http://joomlacode.org/gf/download/frsrelease/16287/70842/Joomla_2.5.0_Beta1-Full_Package.zip";
+	public $cdn_link    	= "http://hwazstor1.blob.core.windows.net/cdn/CDN.zip";
+	public $sitename    	= "Joomla on Azure";
+	public $mailfrom    	= "info@microsoft.com";
+	public $fromname    	= "Joomla@Microsoft";
+	public $session_handler = "database";
+	public $metakeys = "Joomla, Cloud, Windows Azure";
+	public $metadesc = "Joomla on Windows Azure";
 }
 ?>
