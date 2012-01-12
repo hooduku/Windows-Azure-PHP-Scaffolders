8f6ab735e706f14ca83c8bb10b71a97efc742f34
sseshachala <sid4it@gmail.com>
2012-01-12 04:34:52 -0600

    Joomla Link updated.

--- Joomla/Joomla/Config.class.php
+++ Joomla/Joomla/Config.class.php
@@ -2,7 +2,7 @@
 
 class Config {
     
-	public $joomla_link 	= "http://joomlacode.org/gf/download/frsrelease/16287/70842/Joomla_2.5.0_Beta1-Full_Package.zip";
+	public $joomla_link 	= "http://hwazstor1.blob.core.windows.net/cdn/Joomla_2.5.0_Beta2-Full_Package.zip";
 	public $cdn_link    	= "http://hwazstor1.blob.core.windows.net/cdn/CDN.zip";
 	public $sitename    	= "Joomla on Azure";
 	public $mailfrom    	= "info@microsoft.com";
