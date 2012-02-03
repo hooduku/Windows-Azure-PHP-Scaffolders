93b25cd6b4cfbc00f8c82bffa60947a26605d68b
sseshachala <sid4it@gmail.com>
2012-02-03 07:53:54 -0600

    Pointed to 2.5.1 GA.

--- Joomla/Joomla/Config.class.php
+++ Joomla/Joomla/Config.class.php
@@ -2,7 +2,7 @@
 
 class Config {
     
-	public $joomla_link 	= "http://joomlacode.org/gf/download/frsrelease/16371/71446/Joomla_2.5.0_RC1-Full_Package.zip";
+	public $joomla_link 	= "http://joomlacode.org/gf/download/frsrelease/16512/72038/Joomla_2.5.1-Stable-Full_Package.zip";
 	public $cdn_link    	= "http://hwazstor1.blob.core.windows.net/cdn/CDN.zip";
 	public $sitename    	= "Joomla on Azure";
 	public $mailfrom    	= "info@microsoft.com";
